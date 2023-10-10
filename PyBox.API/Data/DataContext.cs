fd5ba5b9804a37e35a5b701bbeea8ec4b060e136
The-Luck-87 <97456769+The-Luck-87@users.noreply.github.com>
2023-10-10 10:22:22 +0200

    First stable release

--- PyBox.API/Data/DataContext.cs
+++ PyBox.API/Data/DataContext.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using PyBox.Shared.Models.Script;
 
 namespace PyBox.API.Data
 {
 	public class DataContext : DbContext
 	{
-		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+			if (!Database.CanConnect())
+			{
+				Database.EnsureCreated();
+				ScriptEntity[] list = JsonConvert.DeserializeObject<ScriptEntity[]>(Properties.Resources.Examples)!;
+				foreach (var script in list)
+				{
+					Scripts!.Add(
+						new ScriptEntity()
+						{
+							CreatedAt = script.CreatedAt,
+							DeletedAt = script.DeletedAt,
+							UpdatedAt = script.UpdatedAt,
+							Description = script.Description,
+							Enabled = script.Enabled,
+							ScriptText = script.ScriptText,
+							Title = script.Title
+						}
+					);
+					SaveChangesAsync();
+				}
+			}
+		}
 		public DbSet<ScriptEntity> Scripts { get; set; }
 
 	}
